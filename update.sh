40e77998ead4502852abfdb98e23118c392c7362
Elkmar <dimitri.jeangerard@gmail.com>
2018-08-26 09:07:24 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,15 +1,16 @@
 cd /home/alqo/ALQO
 cd /home/alqo/alqo
 
-wget -N https://github.com/ALQOCRYPTO/ALQO/releases/download/v3.5/alqo-cli
-chmod -f 777 alqo-cli
 ./alqo-cli stop
 
 echo "Waiting 20 seconds before continuing"
 sleep 20
+apt-get install -y libevent-dev
 
+wget -N https://github.com/ALQOCRYPTO/ALQO/releases/download/ATLAS/alqo-cli
+chmod -f 777 alqo-cli
 
-wget -N https://github.com/ALQOCRYPTO/ALQO/releases/download/v3.5/alqod
+wget -N https://github.com/ALQOCRYPTO/ALQO/releases/download/ATLAS/alqod
 chmod -f 777 alqod
 
 /home/alqo/ALQO/alqod -reindex
