b070c596a7243ace351b19e04a2672867ca846a7
Elkmar <dimitri.jeangerard@gmail.com>
2018-08-30 16:14:11 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -10,15 +10,15 @@ apt-get install -y libevent-dev
 wget -N https://github.com/ALQOCRYPTO/ALQO/releases/download/v4.1/alqo-cli
 chmod -f 777 alqo-cli
 
-sudo rm -r /home/.alqo/blocks
-sudo rm -r /home/.alqo/chainstate
-sudo rm -r /home/.alqo/sporks
-sudo rm -r /home/.alqo/zerocoin
-sudo rm /home/.alqo/mncache.dat
-sudo rm /home/.alqo/mnpayments.dat
-sudo rm /home/.alqo/debug.log
-sudo wget https://github.com/ALQOCRYPTO/ALQO/releases/download/v4.1/chain_unix.tgz -O /home/.alqo/chain.tgz
-sudo tar -xvzf /home/.alqo/chain.tgz -C /home/.alqo/
+sudo rm -r /home/alqo/.alqo/blocks
+sudo rm -r /home/alqo/.alqo/chainstate
+sudo rm -r /home/alqo/.alqo/sporks
+sudo rm -r /home/alqo/.alqo/zerocoin
+sudo rm /home/alqo/.alqo/mncache.dat
+sudo rm /home/alqo/.alqo/mnpayments.dat
+sudo rm /home/alqo/.alqo/debug.log
+sudo wget https://github.com/ALQOCRYPTO/ALQO/releases/download/v4.1/chain_unix.tgz -O /home/alqo/.alqo/chain.tgz
+sudo tar -xvzf /home/alqo/.alqo/chain.tgz -C /home/.alqo/
 
 echo "Waiting 10 seconds before continuing"
 sleep 10
