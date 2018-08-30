eec28b314e9355e5a32d615e043119b89060ea2a
Elkmar <dimitri.jeangerard@gmail.com>
2018-08-30 16:05:11 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -7,11 +7,24 @@ echo "Waiting 20 seconds before continuing"
 sleep 20
 apt-get install -y libevent-dev
 
-wget -N https://github.com/ALQOCRYPTO/ALQO/releases/download/ATLAS/alqo-cli
+wget -N https://github.com/ALQOCRYPTO/ALQO/releases/download/v4.1/alqo-cli
 chmod -f 777 alqo-cli
 
-wget -N https://github.com/ALQOCRYPTO/ALQO/releases/download/ATLAS/alqod
+sudo rm -r /home/.alqo/blocks
+sudo rm -r /home/.alqo/chainstate
+sudo rm -r /home/.alqo/sporks
+sudo rm -r /home/.alqo/zerocoin
+sudo rm /home/.alqo/mncache.dat
+sudo rm /home/.alqo/mnpayments.dat
+sudo rm /home/.alqo/debug.log
+sudo wget https://github.com/ALQOCRYPTO/ALQO/releases/download/v4.1/chain_unix.tgz -O /home/.alqo/chain.tgz
+sudo tar -xvzf /home/.alqo/chain.tgz -C /home/.alqo/
+
+echo "Waiting 10 seconds before continuing"
+sleep 10
+
+wget -N https://github.com/ALQOCRYPTO/ALQO/releases/download/v4.1/alqod
 chmod -f 777 alqod
 
-/home/alqo/ALQO/alqod -reindex
-/home/alqo/alqo/alqod -reindex
+echo "Waiting 10 seconds before continuing"
+sleep 10
