3474ae01e49ee26bc5f4df7c992d54dc38f12e1c
Elkmar <dimitri.jeangerard@gmail.com>
2019-12-31 12:07:45 +0100

    Installation shell file

--- install.sh
+++ install.sh
@@ -1,12 +1,23 @@
+#create an alqo directory
 mkdir alqo
 cd alqo
-wget https://builds.alqo.org/linux/alqod &&chmod -f 777 alqod
-wget https://builds.alqo.org/linux/alqo-cli &&chmod -f 777 alqo-cli
-./alqod
+
+#download ALQO version 6.3.00
+wget https://github.com/ALQO-Universe/ALQO/releases/download/v6.3.0.0-c7fc25cad/ALQO-v6.3.0.0-c7fc25cad-lin64.tgz
+
+#extract the archive and erase the archive
+tar xvf ALQO-v6.3.0.0-c7fc25cad-lin64.tgz
+rm ALQO-v6.3.0.0-c7fc25cad-lin64.tgz
+
+#run alqo client to create the conf files then stop it 
+cd ALQO-v6.3.0.0-c7fc25cad-lin64
+./alqod -daemon
 ./alqo-cli stop
-cd
-cd ./.alqo
+
+#go to conf file in order to edit
+cd /root/.alqocrypto
 nano ./alqo.conf
-cd
-cd alqo
-./alqod
+
+#run the client again
+cd /root/alqo/ALQO-v6.3.0.0-c7fc25cad-lin64
+./alqod -daemon
